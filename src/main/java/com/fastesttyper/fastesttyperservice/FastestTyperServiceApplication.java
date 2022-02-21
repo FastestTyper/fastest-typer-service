aa7cfab533d358f33c41f977b1480a5a9707ba8a
tronxi <sergio.gsanchez97@gmail.com>
2022-02-21 17:50:14 +0100

    initial commit

---
+++ src/main/java/com/fastesttyper/fastesttyperservice/FastestTyperServiceApplication.java
@@ -0,0 +1,13 @@
+package com.fastesttyper.fastesttyperservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FastestTyperServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FastestTyperServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/fastesttyper/fastesttyperservice/FastestTyperServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fastesttyper.fastesttyperservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FastestTyperServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
